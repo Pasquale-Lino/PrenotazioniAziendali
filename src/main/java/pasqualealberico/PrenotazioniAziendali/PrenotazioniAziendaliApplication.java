e60b7eb868b763ab38270323c3d26618f19414e8
Pasquale-Lino <pasquale.alberico94@gmail.com>
2025-10-17 11:21:35 +0200

    Initial commit

---
+++ src/main/java/pasqualealberico/PrenotazioniAziendali/PrenotazioniAziendaliApplication.java
@@ -0,0 +1,13 @@
+package pasqualealberico.PrenotazioniAziendali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotazioniAziendaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenotazioniAziendaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pasqualealberico/PrenotazioniAziendali/PrenotazioniAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package pasqualealberico.PrenotazioniAziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotazioniAziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
